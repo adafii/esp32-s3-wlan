360f2cc26d1806c61d14cf8004ce985a29471e90
Liia Rimpiläinen <liia@oru.fi>
2024-03-28 21:59:36 +0200

    WiFi connection attempt

--- components/wlan/wlan.c
+++ components/wlan/wlan.c
@@ -1,8 +1,12 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
+#include "freertos/event_groups.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "nvs_flash.h"
 #include <stdio.h>
+#include <string.h>
 
 static const char* TAG = "wlan";
 
@@ -10,15 +14,15 @@ static const char* TAG = "wlan";
 typedef struct {
     const char* ssid;
     const char* wpa_psk;
-} wlan_config_t;
+} nvs_wifi_config_t;
 
 esp_err_t init();
 esp_err_t scan(wifi_ap_record_t** ap_records, uint16_t* ap_num);
-esp_err_t connect();
+esp_err_t connect(const nvs_wifi_config_t* config);
 
 char const* get_auth_type(wifi_auth_mode_t auth);
 void print_ap_record(const wifi_ap_record_t ap_records[], const uint16_t* ap_num);
-esp_err_t get_wlan_config(wlan_config_t* wlan_config);
+esp_err_t get_wlan_config(nvs_wifi_config_t* wlan_config);
 
 // Auth mode type to string
 char const* get_auth_type(wifi_auth_mode_t auth) {
@@ -117,7 +121,7 @@ char* read_nvs_str(const nvs_handle_t* handle, const char* key) {
     return str;
 }
 
-esp_err_t get_wlan_config(wlan_config_t* wlan_config) {
+esp_err_t get_wlan_config(nvs_wifi_config_t* wlan_config) {
     nvs_handle_t nvs_handle = {};
     ESP_ERROR_CHECK(nvs_open("wlan_config", NVS_READONLY, &nvs_handle));
 
@@ -127,7 +131,63 @@ esp_err_t get_wlan_config(wlan_config_t* wlan_config) {
     return ESP_OK;
 }
 
-esp_err_t connect() {
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+static int s_retry_num = 0;
+static EventGroupHandle_t s_wifi_event_group;
+
+static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 4) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+esp_err_t connect(const nvs_wifi_config_t* config) {
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    esp_event_handler_instance_t instance_any_id = {};
+    esp_event_handler_instance_t instance_got_ip = {};
+
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+    wifi_config_t wifi_config = {.sta.threshold.authmode = WIFI_AUTH_WPA_WPA2_PSK};
+    strncpy((char*)(&wifi_config.sta.ssid), config->ssid, 32);
+    strncpy((char*)(&wifi_config.sta.password), config->wpa_psk, 32);
+
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    EventBits_t bits =
+        xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID: %s", config->ssid);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID: %s", config->ssid);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
     return ESP_OK;
 }
 
@@ -136,12 +196,11 @@ void test() {
     wifi_ap_record_t* ap_records = NULL;
     uint16_t ap_num = 0;
 
-    scan(&ap_records, &ap_num);
+/*    scan(&ap_records, &ap_num);
     print_ap_record(ap_records, &ap_num);
-    free(ap_records);
+    free(ap_records);*/
 
-    /*    wlan_config_t wlan_config = {};
-        get_wlan_config(&wlan_config);
-        printf("SSID: %s\n", wlan_config.ssid);
-        printf("WPA PSK: %s\n", wlan_config.wpa_psk);*/
+    nvs_wifi_config_t nvs_wifi_config = {};
+    get_wlan_config(&nvs_wifi_config);
+    ESP_ERROR_CHECK(connect(&nvs_wifi_config));
 }
